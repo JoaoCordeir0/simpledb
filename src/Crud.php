e6e902c8fbb87a586bbd0aee83e0974f07c94c26
joao.cordeiro <joao.victor@nubbi.com.br>
2024-03-07 11:13:13 -0300

    Melhorias

---
+++ src/Crud.php
@@ -0,0 +1,74 @@
+<?php 
+
+namespace SimpleDB;
+
+use Exception;
+
+class Crud 
+{ 
+    private $oper;
+    
+    public function __construct(object $oper)
+    {
+        $this->oper = $oper;       
+    }
+    
+    public function insertDB() 
+    {
+        try 
+        {
+            $interrogation = substr(str_repeat('?,', count($this->oper->getColumns())), 0, -1); ;        
+
+            $query = "INSERT INTO " . $this->oper->getTable() . " (" . Helper::unpackColumns($this->oper->getColumns()) . ") VALUES (" . $interrogation . ")";       
+            
+            $insert = $this->oper->getConn()->prepare($query);        
+    
+            $c = 1;
+            foreach ($this->oper->getColumns() as $column) 
+            {
+                $col = explode(':', $column);            
+                $insert->bindParam($c, $this->oper->getData()[$col[0]], $col[2] == 'int' ? \PDO::PARAM_INT : \PDO::PARAM_STR);                
+                $c++;
+            }        
+            $insert->execute();
+        }
+        catch (Exception $e)
+        {
+            print $e->getMessage();
+        }
+    }      
+
+    public function updateDB() 
+    {
+        echo "Linha atualizada na tabela -> " . $this->oper->getTable() . PHP_EOL;
+    } 
+
+    public function deleteDB() 
+    {
+        echo "Linha deletada na tabela -> " . $this->oper->getTable() . PHP_EOL;
+    } 
+
+    public function selectDB() 
+    {   
+        try 
+        {
+            $orderasc = strlen($this->oper->getOrderasc()) > 0 ? ' ORDER BY ' . $this->oper->getOrderasc() . ' ASC' : '';     
+            $orderdesc = strlen($this->oper->getOrderdesc()) > 0 ? ' ORDER BY ' . $this->oper->getOrderdesc() . ' DESC' : '';     
+            $order = $orderasc . $orderdesc;
+    
+            $query = "SELECT id, " . Helper::unpackColumns($this->oper->getColumns()) . " FROM " . $this->oper->getTable() . Helper::unpackWhere($this->oper->getWhere(), $this->oper->getOrwhere(), $this->oper->getLike()) . $order . Helper::unpackLimit($this->oper->getLimit());
+            $query = str_replace('  ', ' ', $query);
+            
+            // print($query . PHP_EOL);
+            
+            $select = $this->oper->getConn()->prepare($query);
+            $select->execute();
+            return (object) $select->fetchAll(\PDO::FETCH_ASSOC);      
+        }
+        catch (Exception $e)
+        {
+            print $e->getMessage();
+        }              
+    } 
+}
+
